fab92924d652c4bf2badf1ae2d1a0637a75091e0
vanndhall <szymon.mantey@wp.pl>
2019-02-24 11:38:17 +0100

    Skończona gra Dodać Wyszukiwanie binarne

--- src/Gra.java
+++ src/Gra.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Gra {
     //GRA
@@ -7,9 +8,10 @@ public class Gra {
     //3. Sprawdz
     //4.opcjonalnie stworzyc zautomatyzowane rozwiazanie uzywajac wyszukiwania binarnego
     public static void main(String[] args) {
-    int x = getRandomInt();
-    Wynik(x);
 
+        int x = getRandomInt();
+        System.out.println("x = "+ x);
+        Wynik(x);
 
     }
 
@@ -20,35 +22,43 @@ public class Gra {
     }
 
     public static int getUserValue(){
+    Scanner scan = new Scanner(System.in);
+        System.out.println("Podaj liczbe: ");
+        return  scan.nextInt();
 
     }
-     public static Integer Wynik(int x){
+     public static void Wynik(int x){
          int liczba = getUserValue();
-        if (x == liczba)
-            System.out.println("Sukcess!");
 
-        if(x> liczba)
-            System.out.println("Liczba jest  mniejsza");
 
-        if(x< liczba)
-            System.out.println("Liczba jest większa");
-
-     }
-    public static int wyszukiwanieBinarne(int szukanaLiczba)
-    {
-        int lewo = 0, prawo = tablicaLiczb.length - 1, srodek = 0;
-
-        while(lewo <= prawo)
-        {
-            srodek = (lewo + prawo) / 2;
-            if(tablicaLiczb[srodek] == szukanaLiczba)
-                return srodek;
-            else if(tablicaLiczb[srodek] < szukanaLiczba)
-                lewo = srodek + 1;
-            else
-                prawo = srodek - 1;
+        if(x> liczba) {
+            System.out.println("Liczba jest  większa");
+            Wynik(x);
         }
 
-        return -1; //jeżeli liczba nie została odnaleziona zwróć -1
-    }
+        else if(x< liczba) {
+            System.out.println("Liczba jest mniejsza");
+            Wynik(x);
+        }
+        else
+            System.out.println("Sukcess!");
+
+     }
+//        public static int wyszukiwanieBinarne(int szukanaLiczba)
+//             {
+//         int lewo = 0, prawo = tablicaLiczb.length - 1, srodek = 0;
+//
+//        while(lewo <= prawo)
+//        {
+//            srodek = (lewo + prawo) / 2;
+//            if(tablicaLiczb[srodek] == szukanaLiczba)
+//                return srodek;
+//            else if(tablicaLiczb[srodek] < szukanaLiczba)
+//                lewo = srodek + 1;
+//            else
+//                prawo = srodek - 1;
+//        }
+//
+//        return -1; //jeżeli liczba nie została odnaleziona zwróć -1
+//    }
 }
